25c0489dc0cf4a5ecdc1f656613f74c20d929e30
Ronnyatp <ronnyatp@hotmail.com>
2025-07-12 22:05:30 -0500

    Mejoras Visuales!!!

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ document.getElementById('formulario').addEventListener('submit', function (e) {
       float pixel_size = length(screenSize.xy) / PIXEL_FILTER;
       vec2 uv = (floor(screen_coords.xy*(1./pixel_size))*pixel_size - 0.5*screenSize.xy)/length(screenSize.xy) - OFFSET;
       float uv_len = length(uv);
-      
+  
       float speed = (SPIN_ROTATION*SPIN_EASE*0.2);
       if(IS_ROTATE){
          speed = iTime * speed;
@@ -41,17 +41,17 @@ document.getElementById('formulario').addEventListener('submit', function (e) {
       float new_pixel_angle = atan(uv.y, uv.x) + speed - SPIN_EASE*20.*(1.*SPIN_AMOUNT*uv_len + (1. - 1.*SPIN_AMOUNT));
       vec2 mid = (screenSize.xy/length(screenSize.xy))/2.;
       uv = (vec2((uv_len * cos(new_pixel_angle) + mid.x), (uv_len * sin(new_pixel_angle) + mid.y)) - mid);
-      
+  
       uv *= 30.;
       speed = iTime*(SPIN_SPEED);
       vec2 uv2 = vec2(uv.x+uv.y);
-      
+  
       for(int i=0; i < 5; i++) {
           uv2 += sin(max(uv.x, uv.y)) + uv;
           uv  += 0.5*vec2(cos(5.1123314 + 0.353*uv2.y + speed*0.131121),sin(uv2.x - 0.113*speed));
           uv  -= 1.0*cos(uv.x + uv.y) - 1.0*sin(uv.x*0.711 - uv.y);
       }
-      
+  
       float contrast_mod = (0.25*CONTRAST + 0.5*SPIN_AMOUNT + 1.2);
       float paint_res = min(2., max(0.,length(uv)*(0.035)*contrast_mod));
       float c1p = max(0.,1. - contrast_mod*abs(1.-paint_res));
@@ -64,6 +64,12 @@ document.getElementById('formulario').addEventListener('submit', function (e) {
   void mainImage(out vec4 fragColor, in vec2 fragCoord) {
       vec2 uv = fragCoord/iResolution.xy;
       fragColor = effect(iResolution.xy, uv * iResolution.xy);
+  }
+  
+  void main() {
+      vec4 color;
+      mainImage(color, gl_FragCoord.xy);
+      gl_FragColor = color;
   }
   `;
   
